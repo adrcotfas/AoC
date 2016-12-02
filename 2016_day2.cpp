05b92d15d39f90bcfaa43ce82cc4fb4f8f9a0a47
Adrian Cotfas <adrcotfas@gmail.com>
2016-12-02 16:06:59 +0200

    commit day2 part 2

--- 2016_day2.cpp
+++ 2016_day2.cpp
@@ -26,15 +26,17 @@ const int buttons[3][3] = { {1,2,3}
                            ,{4,5,6}
                            ,{7,8,9} };
 
-int main() {
-    std::vector<std::string> instructions = extractInput("c:/input.txt");
-    int currentX = 1;
-    int currentY = 1;
+std::vector<int> getPassword(const std::vector<std::string>& instructions,
+                             const int initialX,
+                             const int initialY) {
 
     std::vector<int> password;
+    int currentX = initialX;
+    int currentY = initialY;
+
     for (auto& instruction : instructions) {
         for (auto& character : instruction) {
-            switch (character){
+            switch (character) {
             case 'U':
                 if (currentY > 0) {
                     --currentY;
@@ -57,7 +59,59 @@ int main() {
                 break;
             }
         }
-        password.push_back(buttons[currentX][currentY]);
+        password.push_back(buttons[currentY][currentX]);
     }
+    return password;
+}
+
+const char fancyButtons[5][5] = { { ' ',' ','1',' ',' ' },
+                                  { ' ','2','3','4',' ' },
+                                  { '5','6','7','8','9' },
+                                  { ' ','A','B','C',' ' },
+                                  { ' ',' ','D',' ',' ' } };
+
+std::vector<char> getFancyPassword(const std::vector<std::string>& instructions,
+    const int initialX,
+    const int initialY) {
+
+    std::vector<char> password;
+    int currentX = initialX;
+    int currentY = initialY;
+
+    for (auto& instruction : instructions) {
+        for (auto& character : instruction) {
+            switch (character) {
+            case 'U':
+                if (fancyButtons[currentY - 1][currentX] != ' ' && currentY > 0) {
+                    --currentY;
+                }
+                break;
+            case 'L':
+                if (fancyButtons[currentY][currentX - 1] != ' ' && currentX > 0) {
+                    --currentX;
+                }
+                break;
+            case 'D':
+                if (fancyButtons[currentY + 1][currentX] != ' ' && currentY < 4) {
+                    ++currentY;
+                }
+                break;
+            case 'R':
+                if (fancyButtons[currentY][currentX + 1] != ' ' && currentX < 4) {
+                    ++currentX;
+                }
+                break;
+            }
+        }
+        password.push_back(fancyButtons[currentY][currentX]);
+    }
+    return password;
+}
+
+int main() {
+    std::vector<std::string> instructions = extractInput("c:/input.txt");
+    std::vector<int> password = getPassword(instructions, 1, 1);
+    std::vector<char> fancyPassword = getFancyPassword(instructions, 0, 2);
+    
     return 0;
 }
